e9061a70d6982c0c9c859be455f9e4a6ef4d8b5c
Evan Porter <evan.porter@dotman.ca>
2013-12-17 12:02:11 -0500

    Adding a view for individual records

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,6 +7,9 @@ app.config(['$routeProvider', function($routeProvider) {
     when('/', {
         controller: 'ContactListCtrl',
         templateUrl: 'views/list.html'
+    }).when('/view/:contactId', {
+        controller: 'ContactCtrl',
+        templateUrl: 'views/view_contact.html'
     }).otherwise({
         redirectTo: '/'
     });
@@ -15,5 +18,11 @@ app.config(['$routeProvider', function($routeProvider) {
 app.controller('ContactListCtrl', ['$scope', '$http', function($scope, $http) {
     $http.get('/contacts').success(function(data, status, headers, config) {
         $scope.contacts = data;
+    });
+}]);
+
+app.controller('ContactCtrl', ['$scope', '$http', '$route', function($scope, $http, $route) {
+    $http.get('/contacts/' + $route.current.params.contactId).success(function(data, status, headers, config) {
+        $scope.contact = data;
     });
 }]);
